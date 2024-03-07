df14c23398c3fb04f5de51482b4cc6b800c1d7bc
unknown <diogo.ribeiro.2808@gmail.com>
2024-03-07 11:36:23 +0000

    add cart total on cart page

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -1,13 +1,16 @@
-import React, { useEffect, usecart } from 'react'; 
+import React, { useEffect, useState, usecart } from 'react'; 
 import Header from '../components/Header.js';
 import { useCart } from '../components/CartProvider.js';
 import Product from '../components/Product.js';
 import getCookie from '../scripts/getCookie.js';
+import '../style/cartPage.css';
 
 function CartPage(){
 
-
   const { cart, dispatch } = useCart();
+  
+  const sum = cart.products.reduce((acc, product, index) => acc + (product.price * cart.quantities[index]), 0).toFixed(2);
+
   const displayContent =  (
     <>
       <Header />
@@ -16,12 +19,14 @@ function CartPage(){
           <li key={product.id}>
             <p>ID: {product.id}</p>
             <p>Name: {product.productName}</p>
+            <p>Price: {product.price}</p>
             <p>Quantity: {cart.quantities[index]}</p>
             <button onClick={() => dispatch({ type: 'ADD', payload: product })}>+</button>
             <button onClick={() => dispatch({ type: 'DELETE', payload: product })}>-</button>
           </li>
         ))}
       </ul>
+      <p id='cartPageTotal'>TOTAL: {sum}</p>
       </>
   )
 
